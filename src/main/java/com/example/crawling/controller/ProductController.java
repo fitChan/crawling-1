c5c34b234a781fb697ac9a84108576135503035a
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2021-07-17 13:15:21 +0900

    화이팅

--- src/main/java/com/example/crawling/controller/ProductController.java
+++ src/main/java/com/example/crawling/controller/ProductController.java
@@ -1,6 +1,31 @@
 package com.example.crawling.controller;
 
+import com.example.crawling.dto.respones.ProductDetailRes;
+import com.example.crawling.dto.respones.ProductSimpleRes;
+import com.example.crawling.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/main")
+    public ResponseEntity<List<ProductSimpleRes>> findSimpleProduct(){
+        return ResponseEntity.ok().body(productService.findSimpleProduct());
+    }
+
+    @GetMapping("/market/products/{productId}")
+    public ResponseEntity<ProductDetailRes> findDetailProduct(@PathVariable(name = "productId") Long id){
+        return ResponseEntity.ok().body(productService.findDetailProduct(id));
+    }
 
 
 }
--- src/main/java/com/example/crawling/service/ProductService.java
+++ src/main/java/com/example/crawling/service/ProductService.java
@@ -6,6 +6,7 @@ import com.example.crawling.dto.respones.ProductSimpleRes;
 import com.example.crawling.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,15 +16,17 @@ import java.util.List;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @Transactional(readOnly = true)
     public List<ProductSimpleRes>findSimpleProduct(){
         List<Product> products = productRepository.findAll();
         return ProductSimpleRes.list(products);
     }
 
+    @Transactional(readOnly = true)
     public ProductDetailRes findDetailProduct(Long id){
        Product products = productRepository.findById(id).orElseThrow(
                ()-> new IllegalArgumentException("아이디를 찾을 수 없습니다")
-       ); 
+       );
         return ProductDetailRes.of(products);
     }
 
